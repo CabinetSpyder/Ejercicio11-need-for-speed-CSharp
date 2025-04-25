8ecaf1c27c4064a923d31e53c7e6f7e16d976d9e
AndresAAV <andres.artaloytia@gmail.com>
2025-04-25 18:57:15 +0200

    Primera iteracion de la solucion

--- need-for-speed/NeedForSpeed.cs
+++ need-for-speed/NeedForSpeed.cs
@@ -1,34 +1,62 @@
 class RemoteControlCar
 {
     // TODO: define the constructor for the 'RemoteControlCar' class
+    public int _speed;
+    public int _batteryDrain;
+    private int _distanceDriven = 0;
+    private int _batteryLeft = 100;
+    public RemoteControlCar(int speed, int batteryDrain){
+        _speed = speed;
+        _batteryDrain = batteryDrain;
+    }
 
     public bool BatteryDrained()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        if(_batteryDrain > _batteryLeft){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public int DistanceDriven()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+        return _distanceDriven;     
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if(!BatteryDrained()){
+            _distanceDriven += _speed;
+            _batteryLeft -= _batteryDrain;
+        }
+
     }
 
     public static RemoteControlCar Nitro()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        return new RemoteControlCar(50, 4);        
     }
 }
 
 class RaceTrack
 {
     // TODO: define the constructor for the 'RaceTrack' class
+    int _distance;
+
+    public RaceTrack(int distance){
+        _distance = distance;
+    }
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
-        throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        int batteryUses = 100/car._batteryDrain;
+        if(batteryUses*car._speed >= _distance)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
     }
 }
